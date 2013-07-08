9e06abe2496099baa7e40dbe6d6b3ce1ef27e667
Dan Lewis <contrarian@unknown14109fd9f86d.gateway.2wire.net>
2013-07-07 21:46:53 -0500

    added text readout in hats

--- app/scripts/controllers/gameView1.js
+++ app/scripts/controllers/gameView1.js
@@ -3,12 +3,18 @@
 angular.module('PrisonAdventureApp')
   .controller('GameViewCtrl1', function ($scope, $timeout, $http) {
     $http.get('scripts/dialogue/test.json').success(function(data){
-      $scope.str = data.text;
-      console.log(typeof $scope.str);
+
+      var str = data.text;
+      $scope.displayString = "";
+      console.log(typeof str);
       $scope.counter = 0;
       $timeout(function recurse(){
-        $scope.counter = $scope.counter + 1;
-        $timeout(recurse, 1000);
-      },1000);
+        if ($scope.counter < str.length){
+          $scope.counter = $scope.counter + 1;
+          $scope.displayString = str.substr(0,$scope.counter);
+          $timeout(recurse, 50);
+        }
+      },50);
+
     });
   });
